e60981c2bc2c3aee4551379815146a1b76eb0ac3
Gehad AbdelAziz <gehad.ahmed2112@gmail.com>
2024-03-13 12:25:37 +0200

    Initial Commit

---
+++ NeomCoffeeApp/NeomCoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeomCoffeeAppApp.swift
+//  NeomCoffeeApp
+//
+//  Created by Gehad AbdElAziz on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct NeomCoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
